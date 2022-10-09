d26f1e7479272a1f04300456e77f29e3cd8edf18
code-tieumomo <code.tieumomo@gmail.com>
2022-10-09 15:22:35 +0700

    Update cosplay homepage

---
+++ cosplay/index.js
@@ -0,0 +1,10 @@
+const menu = document.querySelector(".menu");
+window.addEventListener("scroll", () => {
+    const position = document.documentElement.scrollTop;
+
+    if (position > 100) {
+        menu.style.backgroundColor = "rgb(17, 17, 17)";
+    } else {
+        menu.style.backgroundColor = "transparent";
+    }
+});
