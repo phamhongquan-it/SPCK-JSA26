a83cb65677db681e773380f8d777c1096ca3d878
Aizuki69 <110227441+Aizuki69@users.noreply.github.com>
2022-10-25 22:21:34 +0700

    cosplay update v6.0

--- cosplay/index.js
+++ cosplay/index.js
@@ -9,3 +9,25 @@ window.addEventListener("scroll", () => {
         menu.style.backgroundColor = "transparent";
     }
 });
+
+const apiPartyInfo = "https://634bbd3cd90b984a1e3e8ce3.mockapi.io/party-info/1";
+
+fetch(apiPartyInfo)
+    .then((r) => r.json())
+    .then((res) => {
+
+        console.log(res);
+
+        const html =
+            `
+        <h1 class="namep4-1">${res.name}</h1>
+        <p class="pp4-1" class="descp4-1"}>${res.desc}</p>
+        <p class="pp4-1" class="addressp4-1">${res.address}</p>
+        <p class="pp4-1" class="timep4-1">${res.time}</p>
+        `;
+
+        document
+            .querySelector("#textboxp4-1")
+            .insertAdjacentHTML("beforeend", html);
+
+    });
